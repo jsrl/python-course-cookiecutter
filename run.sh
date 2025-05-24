f5c75773b2262c5356ac5964f3d8db567b98b41d
jsrl <jl1284@gmail.com>
2025-05-24 16:40:27 +0200

    latest changes

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 function install {
     python -m pip install --upgrade pip
     python -m pip install --upgrade setuptools
-    python -m pip install cookiecutter
+    python -m pip install cookiecutter pytest
 }
 
 function generate-project {
---
+++ tests/test__generate_project.py
@@ -0,0 +1,3 @@
+def test__can_generate_project():
+    """Test that the project can be generated without errors."""
+    assert False
